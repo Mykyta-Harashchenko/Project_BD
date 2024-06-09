99d63492471f87d28c234418f1877d5baa4cdd85
Mykyta-Harashchenko <160050977+Mykyta-Harashchenko@users.noreply.github.com>
2024-06-09 14:12:00 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,47 +1,34 @@
--- Table: students
-DROP TABLE IF EXISTS students;
-CREATE TABLE students (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    student_name VARCHAR(255) UNIQUE NOT NULL,
-    student_surname VARCHAR(255) UNIQUE NOT NULL,
-    group_id INTEGER
+DROP TABLE IF EXISTS [groups];
+CREATE TABLE [groups] (
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+name STRING UNIQUE
 );
 
--- Table: groups
-DROP TABLE IF EXISTS groups;
-CREATE TABLE groups (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    group_name VARCHAR(255) UNIQUE NOT NULL,
-    student_id INTEGER,
-    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- Table: teachers
 DROP TABLE IF EXISTS teachers;
 CREATE TABLE teachers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    teacher_name  VARCHAR(255) UNIQUE NOT NULL,
-    teacher_surname  VARCHAR(255) UNIQUE NOT NULL,
-    group_id INTEGER,
-    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE ON UPDATE CASCADE
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+fullname STRING
 );
 
--- Table: subjects
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+fullname STRING,
+group_id REFERENCES [groups] (id)
+);
+
 DROP TABLE IF EXISTS subjects;
 CREATE TABLE subjects (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subject_name  VARCHAR(255) UNIQUE NOT NULL,
-    teacher_id INTEGER,
-    FOREIGN KEY (teacher_id) REFERENCES teachers(id) ON DELETE CASCADE ON UPDATE CASCADE
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+subject_name STRING,
+teacher_id REFERENCES teachers (id)
 );
 
--- Table: marks
 DROP TABLE IF EXISTS marks;
 CREATE TABLE marks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subject_name  VARCHAR(255) UNIQUE NOT NULL,
-    subject_mark INTEGER,
-    date_of DATE NOT NULL,
-    student_id INTEGER,
-    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE ON UPDATE CASCADE
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+subject_id REFERENCES subjects (id),
+student_id REFERENCES students (id),
+mark INTEGER,
+date_of DATE
 );
